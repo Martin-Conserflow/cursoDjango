8673fb45b7ff28ddaa38699df7d174b0270fb480
unknown <martin.lara@conserflow.com>
2024-08-06 12:58:31 -0600

    Enlistar, Crear, Actualiza, Borrar

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView 
+from .views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDelateView
 
 app_name="blog"
 
 urlpatterns = [
     path('', BlogListView.as_view(), name="home"),
-    path('create/', BlogCreateView.as_view(), name="create")
+    path('create/', BlogCreateView.as_view(), name="create"),
+    path('<int:pk>/', BlogDetailView.as_view(), name="detail"),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name="update"),
+    path('<int:pk>/delete/', BlogDelateView.as_view(), name="delete"),
 ]
